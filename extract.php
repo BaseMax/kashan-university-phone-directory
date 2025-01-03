0e9d60a3b0192ef14d494681e4c52c727b14f4ac
Max Base <MaxBaseCode@Gmail.Com>
2025-01-03 10:23:53 +0100

    Create extract.php

---
+++ extract.php
@@ -0,0 +1,37 @@
+<?php
+$htmlFile = __DIR__ . "/demo.html";
+$jsonFile = __DIR__ . "/output.json";
+
+if (!file_exists($htmlFile)) {
+    die("File not found");
+}
+
+$html = file_get_contents($htmlFile);
+$pattern = '/<tbody>(.*?)<\/tbody>/s';
+$trPattern = '/<tr.*?>(.*?)<\/tr>/s';
+$tdPattern = '/<td.*?>(.*?)<\/td>/s';
+
+$items = [];
+if (preg_match($pattern, $html, $matches)) {
+    $tbodyContent = $matches[1];
+
+    preg_match_all($trPattern, $tbodyContent, $trMatches);
+
+    foreach ($trMatches[0] as $tr) {
+        preg_match_all($tdPattern, $tr, $tdMatches);
+
+        unset($tdMatches[1][0]);
+
+        $tdMatches[1] = array_values($tdMatches[1]);
+
+        $items[] = $tdMatches[1];
+    }
+}
+
+print_r($items);
+
+print "Number of items: " . count($items) . PHP_EOL;
+
+file_put_contents($jsonFile, json_encode($items, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+
+print "Data extracted successfully" . PHP_EOL;
